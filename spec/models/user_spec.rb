6bffb2c0f64c465e301215ed35de78cb1b7a0911
Yael <yael.s@fiverr.com>
2014-01-01 11:51:54 +0200

    finishing sign in out mostly test edits

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,8 @@ describe User do
   
   subject {@user}
 
-  it {should respond_to(:name)}
-  it {should respond_to(:email)}
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
