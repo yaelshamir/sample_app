4c9d9797d49524e778309524caeb8381845b7520
Yael <yael.s@fiverr.com>
2014-01-06 13:48:29 +0200

    finishing chapter 11.1.4 monday

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
 	before_save { email.downcase! }
 	before_create :create_remember_token
 
@@ -25,6 +27,18 @@ class User < ActiveRecord::Base
 	def feed
     	# This is preliminary. See "Following users" for the full implementation.
     	Micropost.where("user_id = ?", id)
+  	end
+
+  	def following?(other_user)
+  		relationships.find_by(followed_id: other_user.id)
+  	end
+
+  	def follow!(other_user)
+  		relationships.create!(followed_id: other_user.id)
+  	end
+
+  	def unfollow!(other_user)
+  		relationships.find_by(followed_id: other_user.id).destroy!  		
   	end
 
 	private
